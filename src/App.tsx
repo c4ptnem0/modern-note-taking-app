72b4c4b1803ca8d3dbb0101a7cfc218a71bed1fb
C4ptnem0 <wawangadvincula123@gmail.com>
2024-12-02 00:43:58 +0800

    added routes for login, signup and dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { useState } from "react";
-import { NoteLayout } from "./components/NoteLayout";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./components/themes/theme-provider";
+import { Login } from "./views/Login";
+import { Signup } from "./views/Signup";
+import { NoteLayout } from "./layouts/NoteLayout";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-        <NoteLayout />
+        <Router>
+          <Routes>
+            <Route path="/dashboard" element={<NoteLayout />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </Router>
       </ThemeProvider>
     </>
   );
