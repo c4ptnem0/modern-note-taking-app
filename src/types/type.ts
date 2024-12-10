42d96ed7f647f7396eb25ae82c64915008148049
C4ptnem0 <wawangadvincula123@gmail.com>
2024-12-11 01:23:44 +0800

    added note props

--- src/types/type.ts
+++ src/types/type.ts
@@ -2,3 +2,12 @@ export type NoteAddProps = {
   isOpen: boolean;
   setIsOpen: (open: boolean) => void;
 };
+
+export type NoteProps = {
+  user: {
+    id: number;
+    fullName: string;
+    email: string;
+    createOn: string;
+  } | null;
+};
