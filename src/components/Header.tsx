f9c28392fb55b3bcd411f3254d19e002f7fede1a
C4ptnem0 <wawangadvincula123@gmail.com>
2024-12-11 01:17:01 +0800

    added logout function

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,93 +21,101 @@ import {
 } from "@/components/ui/tooltip";
 import { DialogClose } from "@radix-ui/react-dialog";
 import { useTheme } from "./themes/theme-provider";
+import { NoteProps } from "@/types/type";
+import { useNavigate } from "react-router-dom";
 
-export function Header() {
+export function Header({ userInfo }): NoteProps["user"] {
+  const navigate = useNavigate();
   const { theme, setTheme } = useTheme();
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+  };
+
   const toggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
   };
 
   return (
-    <>
-      <div className="sticky top-0 z-50">
-        <div
-          className={`flex items-center justify-between px-6 py-2 drop-shadow ${
-            theme === "light" ? "bg-white" : "bg-black"
+    <div className="sticky top-0 z-50">
+      <div
+        className={`flex items-center justify-between px-6 py-2 drop-shadow ${
+          theme === "light" ? "bg-white" : "bg-black"
+        }`}
+      >
+        <h2
+          className={`text-xl font-medium py-2 ${
+            theme === "light" ? "text-gray-900" : "text-white"
           }`}
         >
-          <h2
-            className={`text-xl font-medium py-2 ${
-              theme === "light" ? "text-gray-900" : "text-white"
-            }`}
-          >
-            Simple Notes
-          </h2>
-          <div className="flex items-center space-x-2 rounded-md w-1/4">
-            <Input type="text" placeholder="Search notes" />
-            <Button type="submit">
-              <Search /> Search
-            </Button>
-          </div>
-          <div className="flex h-6 items-center space-x-3">
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            <Label>Joshua Advincula</Label>
-            <Separator orientation="vertical" />
-            <div>
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <Button variant="ghost" size="icon" onClick={toggleTheme}>
-                      {theme === "light" ? <MoonStarIcon /> : <Sun />}
-                    </Button>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    {theme === "light" ? <p>Dark mode</p> : <p>Light mode</p>}
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
+          Simple Notes
+        </h2>
+        <div className="flex items-center space-x-2 rounded-md w-1/4">
+          <Input type="text" placeholder="Search notes" />
+          <Button type="submit">
+            <Search /> Search
+          </Button>
+        </div>
+        <div className="flex h-6 items-center space-x-3">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <Label>{userInfo?.fullName}</Label>
+          <Separator orientation="vertical" />
+          <div>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button variant="ghost" size="icon" onClick={toggleTheme}>
+                    {theme === "light" ? <MoonStarIcon /> : <Sun />}
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  {theme === "light" ? <p>Dark mode</p> : <p>Light mode</p>}
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
 
-              <Dialog>
-                <DialogTrigger>
-                  <TooltipProvider>
-                    <Tooltip>
-                      <TooltipTrigger asChild>
-                        <Button variant="ghost" size="icon">
-                          <LogOutIcon />
-                        </Button>
-                      </TooltipTrigger>
-                      <TooltipContent>
-                        <p>Logout</p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                </DialogTrigger>
-                <DialogContent className="sm:max-w-[425px]">
-                  <DialogHeader>
-                    <DialogTitle>Are you absolutely sure?</DialogTitle>
-                    <DialogDescription>
-                      This action cannot be undone. This will permanently delete
-                      your account and remove your data from our servers.
-                    </DialogDescription>
-                  </DialogHeader>
-                  <DialogFooter className="justify-end">
-                    <DialogClose asChild>
-                      <Button type="submit" variant="outline">
-                        Cancel
+            <Dialog>
+              <DialogTrigger>
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Button variant="ghost" size="icon">
+                        <LogOutIcon />
                       </Button>
-                    </DialogClose>
-                    <Button type="submit">Logout</Button>
-                  </DialogFooter>
-                </DialogContent>
-              </Dialog>
-            </div>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>Logout</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>Are you absolutely sure?</DialogTitle>
+                  <DialogDescription>
+                    This action cannot be undone. This will permanently delete
+                    your account and remove your data from our servers.
+                  </DialogDescription>
+                </DialogHeader>
+                <DialogFooter className="justify-end">
+                  <DialogClose asChild>
+                    <Button type="submit" variant="outline">
+                      Cancel
+                    </Button>
+                  </DialogClose>
+                  <Button type="submit" onClick={handleLogout}>
+                    Logout
+                  </Button>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
           </div>
         </div>
-        <Separator />
       </div>
-    </>
+      <Separator />
+    </div>
   );
 }
