9662b15a0cfbbb8624588008f23ec6e9dcdeacc4
C4ptnem0 <wawangadvincula123@gmail.com>
2024-12-10 20:51:28 +0800

    added routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import { ThemeProvider } from "./components/themes/theme-provider.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Routes } from "./routes/routers.tsx";
+
+const routes = createBrowserRouter(Routes);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <RouterProvider router={routes} />
+    </ThemeProvider>
   </StrictMode>
 );
