fc28df7eab19aed9539da1cb02f0b68cbb26e93e
C4ptnem0 <wawangadvincula123@gmail.com>
2024-12-11 01:18:42 +0800

    added get user info function

--- src/layouts/NoteLayout.tsx
+++ src/layouts/NoteLayout.tsx
@@ -8,15 +8,39 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { NoteAdd } from "../components/NoteAdd";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Header } from "@/components/Header";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "@/utils/axiosInstance";
+import { NoteProps } from "@/types/type";
 
 export function NoteLayout() {
   const [isOpen, setIsOpen] = useState(false);
+  const [userInfo, setUserInfo] = useState<NoteProps["user"]>();
+  const navigate = useNavigate();
+
+  // Get Logged in user info
+  const getUserInfo = async () => {
+    try {
+      const response = await axiosInstance.get<NoteProps>("/get-user");
+      if (response.data && response.data.user) {
+        setUserInfo(response.data.user);
+      }
+    } catch (error) {
+      if (error?.response.status === 401) {
+        localStorage.clear();
+        navigate("/");
+      }
+    }
+  };
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
 
   return (
     <>
-      <Header />
+      <Header userInfo={userInfo} />
       <div className="container mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8 mt-8 ml-10 mr-10 mb-8">
           <NoteCard />
